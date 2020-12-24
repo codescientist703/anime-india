068e6581edd5287c5317e374b7a0e5dda33a3258
codescientist703 <pukimonlegend@gmail.com>
2020-12-24 17:56:09 +0530

    added asgi

--- animeSite/asgi.py
+++ animeSite/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animeSite.settings')
+settings_module = "animeSite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'animeSite.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_asgi_application()
