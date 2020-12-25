1745bc0fb9e1dfc4d14b408349bb3fc9522707c1
codescientist703 <pukimonlegend@gmail.com>
2020-12-25 16:56:15 +0530

    changed debug

--- animeSite/settings.py
+++ animeSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e4io4-+#wc3sb=h)#y@y3cg^^ao6#@44cy*y0p2*s^a94-n9gm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
