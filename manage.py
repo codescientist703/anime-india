6a4b7078a219e4217e13f29bb1f06126285f1669
codescientist703 <pukimonlegend@gmail.com>
2021-04-20 20:26:17 +0530

    changed manage.py

--- manage.py
+++ manage.py
@@ -6,8 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = "animeSite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'animeSite.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'animeSite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
